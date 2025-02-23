db768cc57d7ab54ad2a8d29640f0ea1c3228829c
refka <ref.kaa2002@gmail.com>
2025-02-23 15:16:41 +0100

    Fix error in hello route

--- hello-devops/test_app.py
+++ hello-devops/test_app.py
@@ -1,8 +1,15 @@
-# test_app.py
 import unittest
 from app import app
+
 class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()  # Créer un client de test
+        self.app.testing = True  # Activer le mode test
+
     def test_home_route(self):
-        tester = app.test_client(self)
-        response = tester.get('/')
-        self.assertEqual(response.status_code, 200)
+        response = self.app.get('/')  # Faire une requête GET à la route principale
+        self.assertEqual(response.status_code, 200)  # Vérifier le code de statut
+        self.assertEqual(response.data.decode(), "Hello, CI/CD")  # Vérifier le contenu de la réponse
+
+if __name__ == "__main__":
+    unittest.main()
