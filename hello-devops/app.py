c64947671d07e2bc3a3c36dd362d439bab3daeef
refka <ref.kaa2002@gmail.com>
2025-02-23 15:05:26 +0100

    Fix error in hello route

--- hello-devops/app.py
+++ hello-devops/app.py
@@ -1,13 +1,14 @@
-# app.py    
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, CI/CD!"
+    return "Hello, CI/CD !"  # Omission de l'exclamation
+
 @app.route('/status')
 def status():
     return "Application is running!"
+
 if __name__ == "__main__":
     app.run(debug=True)
